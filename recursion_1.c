686b8d89ff5c69b661b3f63d03442959e2cf966d
prasenjitghose36 <prasenjitghose36@gmail.com>
2020-05-13 20:25:05 +0530

    updated on 13-05-2020 recursion start

---
+++ recursion_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        fun(n-1);
+        printf("%d \n",n);
+    }
+}
+
+int main()
+{
+    int x=3;
+
+    fun(x);
+    return 0;
+}
---
+++ simple.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("\nEnter a Number:\n");
+    scanf("%d",&a);
+    printf("\nThe Entered Number is: %d \n",a);
+    return 0;
+}
